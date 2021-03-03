0da08350768682afa959b95598c966c2beb0b236
Nana <72888369+nanaama527@users.noreply.github.com>
2021-03-03 13:52:24 -0500

    done

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,17 +1,33 @@
-require 'pry'
+#require 'pry'
 
 def my_all?(collection)
     i = 0
     block_return_values = []
     while i < collection.length
-        block_return_values << yield(collection[i])
-        i = i + 1
+      block_return_values << yield(collection[i])
+      i = i + 1
     end
-binding.pry
-my_all?([1,2,3]) {|i| i < 2}
-if block_return_values.include?(false)
-    false
-else
-    true
-end
-end
+  
+    if block_return_values.include?(false)
+      false
+    else
+      true
+    end
+  end 
+  
+
+# # def my_all?(collection)
+#     i = 0
+#     block_return_values = []
+#     while i < collection.length
+#         block_return_values << yield(collection[i])
+#         i = i + 1
+#     end
+# binding.pry
+# my_all?([1,2,3]) {|i| i < 2}
+# if block_return_values.include?(false)
+#     false
+# else
+#     true
+# end
+# end
